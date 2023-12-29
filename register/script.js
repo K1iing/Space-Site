1c461a16e27ca5f610f1b55541a815214eb4cdef
Gabriel <tyrodunuts@gmail.com>
2023-12-28 23:43:35 -0300

    atualização de registros

--- register/script.js
+++ register/script.js
@@ -1,12 +1,32 @@
 const botao = document.getElementById('inputRegis');
+const email = document.getElementById('emaill');
+const passwordField = document.getElementsByClassName('entrarsenha')[0];
+const passwordConfirmationField = document.getElementsByClassName('confirmarsenha')[0];
 
-// Adiciona um listener para o evento de clique no botão
 botao.addEventListener('click', function() {
-    // Seleciona o parágrafo
+    const password = passwordField.value;
+    const passwordConfirmation = passwordConfirmationField.value;
+    const userEmail = email.value.trim(); // Remove espaços em branco antes e depois do email
+
     const paragrafo = document.getElementById('meuParagrafo');
+    const emailValido = /\S+@\S+\.\S+/.test(userEmail); // Expressão regular para verificar o formato de email
 
-    // Altera o conteúdo do parágrafo
-    paragrafo.textContent = 'Registrado com sucesso';
+    if (password && passwordConfirmation && userEmail !== '') {
+        if (emailValido) {
+            paragrafo.textContent = 'Registrado com sucesso';
 
-    // Você pode fazer outras manipulações de HTML aqui dentro desta função
+            // Limpa os valores dos campos após o registro bem-sucedido
+            passwordField.value = '';
+            passwordConfirmationField.value = '';
+            email.value = ''; // Limpa o campo de e-mail
+        } else {
+            paragrafo.textContent = 'Por favor, insira um e-mail válido';
+        }
+    } else if (!password) {
+        paragrafo.textContent = 'Por favor, insira uma senha';
+    } else if (userEmail === '') {
+        paragrafo.textContent = 'Por favor, insira um e-mail';
+    } else {
+        paragrafo.textContent = 'Senha Não Confere';
+    }
 });
